4c9dc0acf6ea84f7158e1df268c95bbfc4827528
ryzhovalex <thed4rkof@gmail.com>
2024-10-15 15:45:07 +0300

    update editor.gd, main.tscn

--- editor.gd
+++ editor.gd
@@ -16,10 +16,25 @@ var _last_pressed_keycode: int = -1
 var _last_inp_time: int = 0
 var _same_inp_delay: int = 100
 var _font_size: int = 32
+
 var _text: String = ""
+# Index of which the caret are positioned right now.
+var _caret: int = 0
 
 @onready var _mode_text = core.find("Info/Mode")
+@onready var _debug_text = core.find("Info/Debug")
 @onready var _font = preload("res://assets/monogram.ttf")
+var line_spacing: float = 20
+
+enum IndentMode {
+	Tab,
+	Space,
+}
+var indent_mode: IndentMode = IndentMode.Tab
+# Actual only for IndentMode.Tab
+var tab_size: int = 10
+# Actual only for IndentMode.Space. How many spaces are inserted per tab.
+var space_indent_amount: int = 4
 
 const KEYCODES = [
 	KEY_ESCAPE,
@@ -64,7 +79,7 @@ const KEYCODES = [
 	KEY_O,
 	KEY_P,
 	KEY_BRACKETLEFT,
-	KEY_BRACERIGHT,
+	KEY_BRACKETRIGHT,
 	KEY_BACKSLASH,
 
 	KEY_A,
@@ -91,6 +106,10 @@ const KEYCODES = [
 	KEY_COMMA,
 	KEY_PERIOD,
 	KEY_SLASH,
+
+	KEY_CTRL,
+	KEY_ALT,
+	KEY_SPACE,
 ]
 const WRITABLE_KEYCODES = {
 	KEY_QUOTELEFT: ["`", "~"],
@@ -106,6 +125,8 @@ const WRITABLE_KEYCODES = {
 	KEY_0: ["0", ")"],
 	KEY_MINUS: ["-", "_"],
 	KEY_EQUAL: ["=", "+"],
+	# Does erasing, so technically is "Writable"
+	KEY_BACKSPACE: ["", ""],
 
 	KEY_TAB: ["\t", "\t"],
 	KEY_Q: ["q", "Q"],
@@ -119,7 +140,7 @@ const WRITABLE_KEYCODES = {
 	KEY_O: ["o", "O"],
 	KEY_P: ["p", "P"],
 	KEY_BRACKETLEFT: ["[", "{"],
-	KEY_BRACERIGHT: ["]", "}"],
+	KEY_BRACKETRIGHT: ["]", "}"],
 	KEY_BACKSLASH: ["\\", "|"],
 
 	KEY_A: ["a", "A"],
@@ -145,13 +166,19 @@ const WRITABLE_KEYCODES = {
 	KEY_COMMA: [",", "<"],
 	KEY_PERIOD: [".", ">"],
 	KEY_SLASH: ["/", "?"],
+
+	KEY_SPACE: [" ", " "],
 }
 
 func _ready() -> void:
 	_mode_text.text = _get_mode_str()
 
 func _draw() -> void:
-	draw_string(_font, Vector2.ZERO, _text, HORIZONTAL_ALIGNMENT_LEFT, -1, _font_size)
+	var lines = _text.split("\n")
+	var pos = Vector2.ZERO
+	for line in lines:
+		draw_string(_font, pos, line, HORIZONTAL_ALIGNMENT_LEFT, -1, _font_size)
+		pos.y += line_spacing
 
 func _get_mode_str():
 	match _mode:
@@ -164,9 +191,33 @@ func _get_mode_str():
 		Mode.Cmd:
 			return "Command"
 
+func _move_caret(offset: int):
+	_caret += offset
+	if _caret < 0:
+		_caret = 0
+	elif _caret > _text.length():
+		_caret = _text.length() - 1
+
+# Write at current caret position.
 func _write(a_text: String):
 	_text += a_text
+	_move_caret(a_text.length())
 	queue_redraw()
+
+# Erase certain amount of characters. If the amount is negative, erase to the
+# left of the caret, otherwise erase to the right.
+func _erase(count: int):
+	if count > 0:
+		_text = _text.erase(_caret, count)
+	else:
+		# We can erase only by positive count, so we move caret to the left by
+		# the required amount, and only then erase.
+		_text = _text.erase(_caret + count, abs(count))
+	_move_caret(count)
+	queue_redraw()
+
+func _debug(a_text: String):
+	_debug_text.text = a_text
 
 func _process_keyboard():
 	_is_shift_pressed = Input.is_key_pressed(KEY_SHIFT)
@@ -174,7 +225,7 @@ func _process_keyboard():
 	_is_alt_pressed = Input.is_key_pressed(KEY_ALT)
 
 	if _last_pressed_keycode == -1:
-		for keycode in range(100):
+		for keycode in KEYCODES:
 			if Input.is_key_pressed(keycode):
 				_last_pressed_keycode = keycode
 				return
@@ -186,17 +237,21 @@ func _process_insert():
 		_last_pressed_keycode = -1
 		return
 
-	var c = ""
-	if _is_shift_pressed:
-		c = WRITABLE_KEYCODES[_last_pressed_keycode][1]
+	if _last_pressed_keycode == KEY_BACKSPACE:
+		_erase(-1)
 	else:
-		c = WRITABLE_KEYCODES[_last_pressed_keycode][0]
-	_write(c)
+		var c = ""
+		if _is_shift_pressed:
+			c = WRITABLE_KEYCODES[_last_pressed_keycode][1]
+		else:
+			c = WRITABLE_KEYCODES[_last_pressed_keycode][0]
+		_write(c)
 	_last_inp_time = core.time()
 	_last_written_keycode = _last_pressed_keycode
 	_last_pressed_keycode = -1
 
 func _physics_process(_delta: float) -> void:
+	_debug("%s" % _caret)
 	if Input.is_key_pressed(KEY_ESCAPE):
 		get_tree().quit()
 	_process_keyboard()
