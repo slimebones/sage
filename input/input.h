b92dde540f2aaaa323da6b3dc32fe59e367b8783
ryzhovalex <thed4rkof@gmail.com>
2024-11-17 14:55:18 +0300

    update input.h

--- input/input.h
+++ input/input.h
@@ -2,4 +2,61 @@
 #include <stdio.h>
 #include "utils.h"
 
+typedef struct Layout {
+    LayoutWritables* writables;
+} Layout;
+
+typedef struct LayoutWritables {
+    char* tilde;
+    char* k1;
+    char* k2;
+    char* k3;
+    char* k4;
+    char* k5;
+    char* k6;
+    char* k7;
+    char* k8;
+    char* k9;
+    char* k0;
+    char* hyphen;
+    char* equal;
+
+    char* q;
+    char* w;
+    char* e;
+    char* r;
+    char* t;
+    char* y;
+    char* u;
+    char* i;
+    char* o;
+    char* p;
+    char* bracket_right;
+    char* bracket_left;
+    char* backslash;
+    char* a;
+    char* s;
+    char* d;
+    char* f;
+    char* g;
+    char* h;
+    char* j;
+    char* k;
+    char* l;
+    char* semicolon;
+    char* z;
+    char* x;
+    char* c;
+    char* v;
+    char* b;
+    char* n;
+    char* m;
+
+    char* comma;
+    char* dot;
+    char* slash;
+    char* space;
+    char* apostrophe;
+} LayoutWritables;
+
 void UpdateInput();
