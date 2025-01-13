2e7d08decdca3e81f0a7a138c43a653aeedd2d6f
ryzhovalex <thed4rkof@gmail.com>
2025-01-13 22:57:51 +0300

    update main.zig

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,18 @@
 const std = @import("std");
+const rl = @import("raylib");
 
 pub fn main() !void {
+    const width = 800;
+    const height = 600;
+
+    rl.initWindow(width, height, "Sage");
+    defer rl.closeWindow();
+    rl.setTargetFPS(60);
+
+    while (!rl.windowShouldClose()) {
+        rl.beginDrawing();
+        defer rl.endDrawing();
+        rl.clearBackground(rl.Color.white);
+        rl.drawText("Hello, world!", 190, 200, 20, rl.Color.light_gray);
+    }
 }
