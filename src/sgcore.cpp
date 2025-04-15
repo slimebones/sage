b9f28b6eaa53692b8edc1171861592fc0be3a6e6
ryzhovalex <thed4rkof@gmail.com>
2025-04-15 09:57:48 +0300

    update .gitattributes, FUNDING.yml, .gitignore, LICENSE, ...

--- src/sgcore.cpp
+++ src/sgcore.cpp
@@ -1,4 +1,7 @@
 #include "sgcore.h"
+#define RAYGUI_IMPLEMENTATION
+#include "raygui/src/raygui.h"
+#undef RAYGUI_IMPLEMENTATION
 
 namespace sgcore {
 
@@ -29,22 +32,33 @@ int init() {
 	return OK;
 }
 
+float relative_height(float value) {
+	return (float)GetScreenHeight()+value;
+}
+
+float relative_width(float value) {
+	return (float)GetScreenWidth()+value;
+}
+
 int loop() {
 	SetTraceLogLevel(LOG_WARNING);
 	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
 	// SetConfigFlags(FLAG_WINDOW_UNDECORATED);
 	InitWindow(800, 450, "Sage");
+	GuiLoadStyle("raygui/styles/dark/style_dark.rgs");
 
 	Image icon = LoadImage("src/icon.png");
 	SetWindowIcon(icon);
 	UnloadImage(icon);
 
+	bool show_message_box = false;
+
 	while (!WindowShouldClose())
 	{
 		BeginDrawing();
+		ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)));
 
-		ClearBackground(BLACK);
-		// DrawTextEx("Congrats! You created your first window!", 190, 200, 20, WHITE);
+		GuiPanel({relative_height(-100), relative_width(-100), 100, 100}, "Test");
 
 		EndDrawing();
 	}
